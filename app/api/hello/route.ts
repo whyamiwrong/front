39fbcdea4c14136c410d0fbfe28415181ff4ba3d
kimbank <kimeunhang@inha.edu>
2023-11-09 04:33:16 +0900

    update

---
+++ app/api/hello/route.ts
@@ -0,0 +1,16 @@
+export async function GET(_request: Request) {
+  // Do whatever you want
+  return new Response("Hello World!", {
+    status: 200,
+  });
+}
+
+/**
+ * @swagger
+ * /hello:
+ *   get:
+ *     description: __\'hello world\'__ 를 반환합니다.
+ *     responses:
+ *       200:
+ *         description: Hello World!
+ */
---
+++ lib/swagger.ts
@@ -0,0 +1,73 @@
+import { createSwaggerSpec } from 'next-swagger-doc';
+
+const info = {
+  title: '맞왜틀 API',
+  version: '0.0.0',
+  // summary: '맞왜틀 API',
+  // description: '맞왜틀 API',
+};
+
+const servers = [
+  {
+    url: 'http://localhost:3000/api',
+    description: 'Local server',
+  },
+  {
+    url: 'https://whyamiwrong-front-29n4bs9zt-kimbank.vercel.app/api',
+    description: 'Production server',
+  },
+  {
+    "url": "https://?????:{port}/{basePath}",
+    "description": "스프링 서버가 될 예정인 것",
+    "variables": {
+      "port": {
+        "enum": [
+          "8080",
+          "443",
+          "80"
+        ],
+        "default": "8080"
+      },
+      "basePath": {
+        "default": "api"
+      }
+    }
+  }
+];
+
+const components = {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+}
+
+export const getApiDocs = async () => {
+  const spec = createSwaggerSpec({
+    apiFolder: 'app/api', // define api folder under app folder
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: '맞왜틀 API',
+        version: '0.0.0',
+        // summary: '맞왜틀 API',
+        // description: '맞왜틀 API',
+      },
+      servers: servers,
+      // components: {
+      //   securitySchemes: {
+      //     BearerAuth: {
+      //       type: 'http',
+      //       scheme: 'bearer',
+      //       bearerFormat: 'JWT',
+      //     },
+      //   },
+      // },
+      security: [],
+    },
+  });
+  return spec;
+};
