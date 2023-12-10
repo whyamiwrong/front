587988896dac5655820b0aa7e728919b504c5ad6
kimbank <kimeunhang@inha.edu>
2023-12-10 21:57:21 +0900

    hotfix: 빌드 에러 수정

--- app/(Back)/api/snack-submissions/route.js
+++ app/(Back)/api/snack-submissions/route.js
@@ -2,99 +2,99 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function correct(submission){
-    const solutions = await prisma.solutions.create({
-        data: {
-            is_correct : 1,
-            submission_id : submission.submission_id,
-        }
-    })
+async function correct(submission) {
+  const solutions = await prisma.solutions.create({
+    data: {
+      is_correct: 1,
+      submission_id: submission.submission_id,
+    },
+  });
 
-    return solutions
+  return solutions;
 }
 
-async function wrong(submission){
-    const solutions = await prisma.solutions.create({
-        data: {
-            is_correct : 0,
-            submission_id : submission.submission_id,
-        }
-    })
+async function wrong(submission) {
+  const solutions = await prisma.solutions.create({
+    data: {
+      is_correct: 0,
+      submission_id: submission.submission_id,
+    },
+  });
 
-    return solutions
+  return solutions;
 }
 
 export async function POST(request) {
+  const { user_id, problem_id, answer } = await request.json();
 
-        const { user_id, problem_id, answer } = await request.json();
+  const submission = await prisma.submissions.create({
+    data: {
+      user: {
+        connect: { user_id: user_id },
+      },
+      problems: {
+        connect: { problem_id: problem_id },
+      },
+      answer: answer,
+    },
+  });
 
-        const submission = await prisma.submissions.create({
-            data: {
-                user:{
-                    connect : {user_id : user_id},
-                },
-                problems:{
-                    connect : {problem_id : problem_id},
-                },
-                answer : answer
-            }
-        })
+  const ox_problems = await prisma.ox_problems.findUnique({
+    where: {
+      problem_id: problem_id,
+    },
+  });
 
-        const ox_problems = await prisma.ox_problems.findUnique({
-            where: {
-                problem_id : problem_id,
-            },
-        })
+  const short_answer_problems = await prisma.short_answer_problems.findUnique({
+    where: {
+      problem_id: problem_id,
+    },
+  });
 
-        const short_answer_problems = await prisma.short_answer_problems.findUnique({
-            where: {
-                problem_id : problem_id,
-            },
-        })
+  const multiple_choices_problems =
+    await prisma.multiple_choices_problems.findUnique({
+      where: {
+        problem_id: problem_id,
+      },
+    });
 
-        const multiple_choices_problems = await prisma.multiple_choices_problems.findUnique({
-            where: {
-                problem_id : problem_id,
-            },
-        })
+  if (
+    ox_problems === null &&
+    short_answer_problems === null &&
+    multiple_choices_problems === null
+  ) {
+    return (
+      Response.json({
+        status: "Error",
+        message: "No matching problem found",
+      }),
+      { status: 404 }
+    );
+  } else if (ox_problems !== null) {
+    if (ox_problems.correct_answer == answer) {
+      correct(submission);
+    } else {
+      wrong(submission);
+    }
+  } else if (short_answer_problems !== null) {
+    if (short_answer_problems.expected_answer == answer) {
+      correct(submission);
+    } else {
+      wrong(submission);
+    }
+  } else if (multiple_choices_problems !== null) {
+    if (multiple_choices_problems.correct_choice == answer) {
+      correct(submission);
+    } else {
+      wrong(submission);
+    }
+  }
 
-        if(ox_problems === null && short_answer_problems === null && multiple_choices_problems === null){
-            return Response.json({
-                status : 'Error',
-                message : 'No matching problem found'
-            }), {status : 404};
-        }
-        else if(ox_problems !== null){
-            if(ox_problems.correct_answer == answer){
-                correct(submission);
-            }
-            else{
-                wrong(submission);
-            }
-        }
-        else if(short_answer_problems !== null){
-            if(short_answer_problems.expected_answer == answer){
-                correct(submission);
-            }
-            else{
-                wrong(submission);
-            }
-        }
-        else if(multiple_choices_problems !== null){
-            if(multiple_choices_problems.correct_choice == answer){
-                correct(submission);
-            }
-            else{
-                wrong(submission);
-            }
-        }
-
-        return Response.json({
-           submissionsid: submission.submission_id,
-           status : 'Success' 
-        });
+  return new Response.json({
+    submissionsid: submission.submission_id,
+    status: "Success",
+  });
 }
-
 
 /**
  * @swagger
@@ -142,5 +142,3 @@ export async function POST(request) {
  *                   type: string
  *                   example: "No matching problem found"
  */
-    
-
