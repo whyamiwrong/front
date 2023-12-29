92785713733bb90c8c2952ab3923af118337b3e2
Jang Seung Beom <wkdtmf357@gmail.com>
2023-12-29 22:04:03 +0900

    feat : 스낵 문제 풀이 결과 제출 api

--- app/(Back)/api/snack/[snackId]/quiz/[quizId]/route.js
+++ app/(Back)/api/snack/[snackId]/quiz/[quizId]/route.js
@@ -16,6 +16,34 @@ export async function GET(req, { params }) {
     return Response.json(snack_quiz);
 }
 
+export async function POST(req, { params }) {
+    const { user_id, snack_quiz_id, selection } = await req.json();
+
+    const snack_quiz = await prisma.snack_quiz.findFirst({
+        where: {
+            snack_quiz_id: snack_quiz_id,
+        },
+    })
+
+    const obj = snack_quiz.selections;
+
+    let is_correct = 0; 
+
+    if(obj.answer == selection){
+        is_correct = 1;
+    }
+
+    const solutions = await prisma.solutions.create({
+        data: {
+            snack_quiz_id : snack_quiz_id,
+            user_id : user_id,
+            is_correct: is_correct,
+        }
+    })
+
+    return Response.json({ user_id, snack_quiz_id, is_correct })
+}
+
 /**
  * @swagger
  * /snack/{snackId}/quiz/{quizId}:
@@ -55,4 +83,49 @@ export async function GET(req, { params }) {
  *               tag:
  *                 1: "자료구조"
  *                 2: "배열"
+ *   post:
+ *     summary: 사용자가 스낵 퀴즈를 제출하고 결과를 반환합니다.
+ *     tags: [Snack]
+ *     parameters:
+ *       - in: path
+ *         name: snackId
+ *         required: true
+ *         description: 퀴즈를 제출할 스낵의 ID
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: quizId
+ *         required: true
+ *         description: 제출할 퀴즈의 ID
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: integer
+ *                 description: 사용자의 ID
+ *               snack_quiz_id:
+ *                 type: integer
+ *                 description: 제출할 스낵 퀴즈의 ID
+ *               selection:
+ *                 type: integer
+ *                 description: 사용자가 선택한 답
+ *     responses:
+ *       200:
+ *         description: 퀴즈 제출 결과
+ *         content:
+ *           application/json:
+ *             example:
+ *               user_id: 1
+ *               snack_quiz_id: 1
+ *               is_correct: 1
+ *       400:
+ *         description: 잘못된 요청 형식
+ *       404:
+ *         description: 요청한 스낵 또는 퀴즈를 찾을 수 없음
  */
