9286050d2e9b88db648c2dc2a618d17b56d4f499
DeepSleep <lyg3715371537@gmail.com>
2024-01-05 00:05:32 +0900

    add correction api

--- app/(Back)/api/problems/[problemId]/route.js
+++ app/(Back)/api/problems/[problemId]/route.js
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-
+import { getVerified } from "@/lib/session";
+//import {code} from "../../../../(Front)/algorithm/[prob_id]/page"
 const prisma = new PrismaClient();
 
+//original problems, problems에서는 
 export async function GET(request, { params }) {
 
         const problem_id = parseInt(params.problemId);
@@ -40,10 +42,30 @@ export async function GET(request, { params }) {
             examples: examples,
             testcases: testcases,
         };
-
+        
         return Response.json(result);
 }
 
+async function hihi(){
+    
+    const username = "whyamiwrong";
+    //const username = getVerified;
+    
+    
+    const solved = await prisma.user.update({
+        where : {
+            username : username,
+            //user_id: 1,//이 부분에 대한 처리는 어떻게 할것인가?
+        },
+        data: {
+            solved:{
+                increment:1,
+            }
+        }
+    })
+    console.log("success")
+    console.log(solved.solved)
+}
 /**
  * @swagger
  * /problems/{problemId}:
@@ -94,3 +116,70 @@ export async function GET(request, { params }) {
  *                         type: string
  */
 
+export async function POST(request, {params,body}){
+    const problem_id = parseInt(params.problemId);
+    const testcases = await prisma.testcases.findUnique({
+        where:{
+            problem_id: problem_id,
+            testcase_id: problem_id,
+        },
+        select:{
+            input: true,
+            output: true
+        }
+        
+    })
+    const input = testcases.input[0];
+    const check = testcases.output[0];
+    //code는 import?해와야 할 것임. 어떤 페이지에서?
+    const code = '#include <iostream>\nusing namespace std;\n\nint main() {\n    cout << "4";\n    return 0;\n}';
+    const runCode = async () => {
+        try{
+            const response = await fetch('https://judge-worker.run.goorm.io/submit/check_answer',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({code, input, check}),
+                credentials: 'include',
+            });
+
+            if(!response.ok) {
+                const error = await response.json();
+                throw new Error(error.detail);
+            }
+            const result = await response.json();
+            return result
+        }
+        catch(error){
+            console.error(error);
+            return {
+                error
+            }
+        }
+    }
+    
+    
+    
+    const result = await runCode({code, input,check});
+    console.log(result)
+    
+    if(result.correct=="1"){
+        console.log("happy")
+        await hihi()
+    }
+    else console.log(result.correct)
+    return Response.json(result)
+
+}
+
+
+// model testcases {
+//     testcase_id       Int               @id @default(autoincrement())
+//     problem_id        Int
+//     input             String            @db.Text
+//     output            String            @db.Text
+//     original_problems original_problems @relation(fields: [problem_id], references: [problem_id], onDelete: NoAction, onUpdate: Restrict, map: "fk_testcases_problems")
+  
+//     @@index([problem_id], map: "fk_testcases_problems")
+//   }
