ad69c911cd964ddbec648ba7ae85eea4c55df33b
kimbank <kimeunhang@inha.edu>
2024-01-05 15:30:29 +0900

    add: hotjar

--- app/(Front)/layout.tsx
+++ app/(Front)/layout.tsx
@@ -1,5 +1,4 @@
-
-import './globals.css';
+import "./globals.css";
 
 import * as React from "react";
 import Box from "@mui/material/Box";
@@ -8,11 +7,7 @@ import ThemeRegistry from "components/ThemeRegistry/ThemeRegistry";
 import Header from "components/auth/header";
 import Menu from "components/shared/menu";
 import Wrapper from "@/components/shared/wrapper";
-import { RecoilRoot } from 'recoil';
-
-
-
-
+import Hotjar from "@/lib/hotjar";
 
 export const metadata = {
   title: "WhyisWrong",
@@ -25,19 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    
     <html lang="ko-KR">
-     
-        <body> 
-         
-          <ThemeRegistry>
-            <Header />
-            {/* <Menu /> */}
-            <Wrapper>{children}</Wrapper>
-          </ThemeRegistry>
-        </body>
- 
+      <head>
+        {process.env.NODE_ENV === "production" && <Hotjar />}
+      </head>
+      <body>
+        <ThemeRegistry>
+          <Header />
+          {/* <Menu /> */}
+          <Wrapper>{children}</Wrapper>
+        </ThemeRegistry>
+      </body>
     </html>
-  
   );
 }
