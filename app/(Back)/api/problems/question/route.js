dc3b3f55cf225eb499fbd48960badb12bc2cfd15
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-01-23 20:23:48 +0900

    feat: 문제 생성 api

---
+++ app/(Back)/api/problems/question/route.js
@@ -0,0 +1,121 @@
+import prisma from "@/lib/prisma";
+import { getVerified } from "@/lib/session";
+
+/*
+{
+    "title": "abcd",
+    "description": "a + b를 작성하세요",
+    "algorithm_category": "수학",
+    "examples": [
+        {
+            "input" : "1 3",
+            "output" : "4"
+        },
+        {
+            "input" : "1 4",
+            "output" : "5"
+        }
+    ],
+    "testcases": [
+        {
+            "input": "1 3",
+            "output": "4"
+        },
+        {
+            "input" : "1 4",
+            "output" : "5"
+        }
+    ]
+}
+*/
+
+export async function POST(req){
+    const { title, description, algorithm_category, example_code, examples, testcases } = await req.json();
+
+    const new_problem = await prisma.problems.create({
+        data: {
+            title: title,
+            description: description,
+            algorithm_category: algorithm_category,
+            example_code: example_code,
+        },
+    });
+
+    for(let i = 0; i < examples.length; i++){
+        const new_examples = await prisma.examples.create({
+            data: {
+                problem_id: new_problem.problem_id,
+                input: examples[i].input,
+                output: examples[i].output,
+            },
+        });
+    };
+
+    for(let i = 0; i < testcases.length; i++){
+        const new_testcases = await prisma.testcases.create({
+            data: {
+                problem_id: new_problem.problem_id,
+                input: testcases[i].input,
+                output: testcases[i].output,
+            },
+        });
+    };
+
+    return Response.json(new_problem);
+}
+
+/**
+ * @swagger
+ * /problems/question:
+ *   post:
+ *     summary: 새로운 문제를 생성합니다.
+ *     tags:
+ *       - Problems
+ *     requestBody:
+ *       description: 새로운 문제에 대한 정보를 담고 있는 JSON Payload
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The title of the problem.
+ *               description:
+ *                 type: string
+ *                 description: The description of the problem.
+ *               algorithm_category:
+ *                 type: string
+ *                 description: The category of the algorithm.
+ *               example_code:
+ *                 type: string
+ *                 description: The example code for the problem.
+ *               examples:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     input:
+ *                       type: string
+ *                       description: The input for an example.
+ *                     output:
+ *                       type: string
+ *                       description: The expected output for the example.
+ *                 description: An array of examples for the problem.
+ *               testcases:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     input:
+ *                       type: string
+ *                       description: The input for a test case.
+ *                     output:
+ *                       type: string
+ *                       description: The expected output for the test case.
+ *                 description: An array of test cases for the problem.
+ *     responses:
+ *       '200':
+ *         description: 성공적인 응답. 생성된 문제에 대한 정보를 반환합니다.
+ */
