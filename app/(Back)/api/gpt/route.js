674d762eb3ae0280fb92926177c42dd5b63576c6
haesoooo <tngoc9087@gmail.com>
2024-01-30 17:10:25 +0900

    gpt api gemini로 변경

--- app/(Back)/api/gpt/route.js
+++ app/(Back)/api/gpt/route.js
@@ -1,37 +1,57 @@
-import OpenAI from 'openai';
 //const dotenv = require('dotenv');
 import prisma from "@/lib/prisma";
 import { getVerified } from "@/lib/session";
+import fs from "fs";
+//const fs = require("fs");
 let extractedData = ''; 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+function fileToGenerativePart(data, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(data).toString("base64"),
+      mimeType
+    },
+  };
+}
+
 
 export async function POST(request) {
   try{
   const session = await request.json();
-  const apiKey = process.env.OPENAI_API_SECRET_KEY;
-  // const prompt = `너는 이제 컴퓨터공학과의 TA야. 학생들을 위한 문제를 내야해. ${session.title}에 대해 묻는 객관식 1문제(4 choices)를 출제 해. 문제의 형식은 반드시 \n 개수도 아래 형식과 똑같아야 해. 두 번 이상 \n을 출력하지마. 정답은 번호만 출력하고, 태그는 2개 이상 ,로 구분해야 해. \n문제 제목:\n문제 설명:\n1. \n2. \n3. \n4. \n정답:\n관련 태그:`;
-  const prompt = `너는 이제 컴퓨터공학과의 TA야. 학생들을 위한 문제를 내야해. '${session.title}'에 대해 묻는 객관식 1문제(4 choices)를 출제 해. 문제의 형식은 반드시 아래 형식과 똑같아야 해. 두 번 이상 \n을 출력하지마. 4가지 선지 중에 정답인 선지는 랜덤으로 정해줘.
-  tag value에는 해당 문제와 관련한 알고리즘 분류를 적어줘.
-  무조건 \`\`\`json으로 시작해\n
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+ 
+  const prompt = `너는 이제 컴퓨터공학과의 TA야. 학생들을 위한 문제를 내야해. 이미지를 분석하고 해당 이미지가 포함하는 내용을 이해하고 이와 관련된 주제에 대해 묻는 객관식 1문제(4 choices)를 출제 해. 문제의 형식은 반드시 아래 형식과 똑같아야 해. 두 번 이상 \n을 출력하지마. 선지를 제시하기 전에 문제 설명에 힌트를 담으면 안돼. 4가지 선지 중에 정답인 선지는 랜덤으로 정해줘. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어줘. 무조건 \`\`\`json으로 시작해\n
   \`\`\`json{"title": "알고리즘 문제 제목","description": "알고리즘 문제 설명","selections": {"1": "선지1","2": "선지2","3": "선지3","4": "선지4","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
-  const openai = new OpenAI({
-    apiKey: apiKey,
-  });
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
-    });
+   const imagePath = "/Users/ihaesu/Desktop/test/learning.png";
+  const imageData = fs.readFileSync(imagePath);
 
-  //   console.log(prompt);
-  // const extractedData = extractInfoFromGPTResponse(response.choices[0].message.content);
+  const imageParts = [
+    fileToGenerativePart(imageData, "image/png"),
+  ];
+  
+  // const uploadedFile = request.files && request.files[0];
+  //   if (!uploadedFile) {
+  //     throw new Error("No file uploaded");
+  //   }
 
+  //   const imageParts = [
+  //     fileToGenerativePart(uploadedFile.content, uploadedFile.mimeType),
+  //   ];
 
-    let extractedData = response.choices[0].message.content;
-    console.log(extractedData);
-    extractedData = extractedData.slice(extractedData.indexOf('{'), extractedData.lastIndexOf('}') + 1);
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
 
-    console.log(extractedData)
-    extractedData = JSON.parse(extractedData);
-    console.log(extractedData);
+    // let extractedData = response.text();
+    // console.log(extractedData);
+    // extractedData = extractedData.slice(extractedData.indexOf('{'), extractedData.lastIndexOf('}') + 1);
+
+    // console.log(extractedData)
+    // extractedData = JSON.parse(extractedData);
+    // console.log(extractedData);
 
     if (extractedData?.answer) {
       extractedData.selections = {...extractedData.selections, answer: extractedData.answer}
@@ -101,6 +121,7 @@ export async function POST(request) {
 //         });
 //     }
 // }
+
 
 
 function extractInfoFromGPTResponse(gptResponse) {
