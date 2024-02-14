c7e5b089c3ac6fe6c7a9131f7b39ea6f16ef8b08
haesoooo <tngoc9087@gmail.com>
2024-02-14 20:51:10 +0900

    gemini 관련 API (hint, review, quiz)

--- app/(Back)/api/gpt/route.js
+++ app/(Back)/api/gpt/route.js
@@ -1,8 +1,6 @@
 //const dotenv = require('dotenv');
 import prisma from "@/lib/prisma";
 import { getVerified } from "@/lib/session";
-import fs from "fs";
-//const fs = require("fs");
 let extractedData = ''; 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
@@ -10,78 +8,89 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 function fileToGenerativePart(data, mimeType) {
   return {
     inlineData: {
-      data: Buffer.from(data).toString("base64"),
+      data: data,
       mimeType
     },
   };
 }
 
-
 export async function POST(request) {
-  try{
+  try {
   const session = await request.json();
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
- 
-  const prompt = `너는 이제 컴퓨터공학과의 TA야. 학생들을 위한 문제를 내야해. 이미지를 분석하고 해당 이미지가 포함하는 내용을 이해하고 이와 관련된 주제에 대해 묻는 객관식 1문제(4 choices)를 출제 해. 문제의 형식은 반드시 아래 형식과 똑같아야 해. 두 번 이상 \n을 출력하지마. 선지를 제시하기 전에 문제 설명에 힌트를 담으면 안돼. 4가지 선지 중에 정답인 선지는 랜덤으로 정해줘. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어줘. 무조건 \`\`\`json으로 시작해\n
-  \`\`\`json{"title": "알고리즘 문제 제목","description": "알고리즘 문제 설명","selections": {"1": "선지1","2": "선지2","3": "선지3","4": "선지4","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
-   const imagePath = "/Users/ihaesu/Desktop/test/learning.png";
-  const imageData = fs.readFileSync(imagePath);
+  const generationConfig = {
+    temperature: 0.9,
+  };
+  var model = '', result = '';
+  if (session.version == 'image') model = genAI.getGenerativeModel({ model: "gemini-pro-vision", generationConfig });
+  else model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
+  // 모델 정의 (session.version에 따라 image or text 모델)
 
-  const imageParts = [
-    fileToGenerativePart(imageData, "image/png"),
+  var prompt = '';
+  if (session.version == 'image' && session.type == 'multi') prompt = `당신은 이제 컴퓨터공학과의 교수입니다. 당신의 임무는 학생들을 위한 컴퓨터 공학 전공 관련 문제를 내는 것입니다. 우선 input으로 제공되는 images들을 분석하고, 모든 이미지들이 포함하고 있는 내용을 이해하여야 합니다. 이미지에서 중요하다고 생각하는 키워드에 대해 묻는 객관식 1문제(4 choices)를 출제해야 합니다. 이미지가 내포하는 keyword + ${session.subject}을 토대로 문제를 출제합니다. 이는 반드시 컴퓨터 공학과 학생이 풀 수 있는 문제여야 합니다. 또한 문제는 단순하지 않고 반드시! 컴퓨터 공학적 개념을 활용하여 풀 법한 난이도여야 합니다. 
+  객관식 문제의 형식은 반드시 아래 형식을 지켜야 합니다. 두 번 이상 \n을 출력하면 안 됩니다. 선지를 제시하기 전에 description에는 선지를 유추할 수 있는 문장을 포함하면 안되고 특정 주제에 대해 묻는 문장이어야 합니다. 문제의 난이도는 컴퓨터 전공 대학생에게 '${session.difficulty}' 정도로 출제해야 합니다. 4가지 선지 중에 정답인 선지는 랜덤으로 정해야 합니다. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어주세요. 반드시! 무조건 json\n으로 시작하세요\n
+  json{"title": "알고리즘 문제 제목","description": "알고리즘 문제 설명","selections": {"1": "선지1","2": "선지2","3": "선지3","4": "선지4","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
+  // 이미지 객관식 문제 출제
+  else if (session.version == 'image' && session.type == 'ox') prompt = `당신은 이제 컴퓨터공학과의 교수입니다. 당신의 임무는 학생들을 위한 컴퓨터 공학 전공 관련 문제를 내는 것입니다. 너는 input으로 제공되는 images들을 분석하고, 이미지에서 중요하다고 생각하는 키워드를 알아내야 합니다. 그리고 키워드에 대해 자세히 묻는 true or false question(정답은 O or X)를 1문제 출제 하세요. 이미지는 ${session.subject}와 관련이 있습니다. 문제의 난이도는 꼭 '${session.difficulty}'하게 내야 합니다. OX 문제의 description에는 해당 context가 거짓인지 참인지에 대해 묻는 내용이 들어가 있어야 합니다. selection 에는 enum type으로 O (true) or X (false)만 들어가야 합니다. OX 문제의 형식은 반드시 아래 형식과 똑같아야 합니다. 두 번 이상 \n을 출력하면 안됩니다. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어주세요. 반드시 json으로 시작하세요\n
+  json{"title": "알고리즘 문제 제목","description": "ox 퀴즈 내용","selections": {"1": "true","2": "false","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
+  // 이미지 OX 문제 출제
+  else if (session.version == 'text' && session.type == 'multi') prompt = `당신은 이제 컴퓨터공학과의 교수입니다. 당신의 임무는 학생들을 위한 컴퓨터 공학 전공 관련 문제를 내는 것입니다. ${session.subject}에 대해 묻는 객관식 1문제(4 choices)를 출제해야 합니다. 이는 반드시 컴퓨터 공학과 학생이 풀 수 있는 문제여야 합니다. 또한 문제는 단순하지 않고 반드시! 컴퓨터 공학적 개념을 활용하여 풀 법한 난이도여야 합니다. 
+  객관식 문제의 형식은 반드시 아래 형식을 지켜야 합니다. 두 번 이상 \n을 출력하면 안 됩니다. 선지를 제시하기 전에 description에는 선지를 유추할 수 있는 문장을 포함하면 안되고 특정 주제에 대해 묻는 문장이어야 합니다. 문제의 난이도는 컴퓨터 전공 대학생에게 '${session.difficulty}' 정도로 출제해야 합니다. 4가지 선지 중에 정답인 선지는 랜덤으로 정해야 합니다. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어주세요. 반드시! 무조건 json\n{으로 시작하세요\n
+  json{"title": "알고리즘 문제 제목","description": "알고리즘 문제 설명","selections": {"1": "선지1","2": "선지2","3": "선지3","4": "선지4","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
+  // 텍스트(키워드) 객관식 문제 출제
+  else if (session.version == 'text' && session.type == 'ox') prompt = `당신은 이제 컴퓨터공학과의 교수입니다. 당신의 임무는 학생들을 위한 컴퓨터 공학 전공 관련 문제를 내는 것입니다. ${session.subject}에 대해 자세히 묻는 true or false question(정답은 O or X)를 1문제 출제 하세요. 문제의 난이도는 꼭 '${session.difficulty}'하게 내야 합니다. OX 문제의 description에는 해당 context가 거짓인지 참인지에 대해 묻는 내용이 들어가 있어야 합니다. selection 에는 enum type으로 O (true) or X (false)만 들어가야 합니다. OX 문제의 형식은 반드시 아래 형식과 똑같아야 합니다. 두 번 이상 \n을 출력하면 안됩니다. tag value에는 해당 문제와 관련한 알고리즘 분류를 적어주세요. 반드시 json\n{ 으로 시작하세요\n
+  json{"title": "알고리즘 문제 제목","description": "ox 퀴즈 내용","selections": {"1": "true","2": "false","answer": "정답인 key"},"tag": {"1": "문제와 관련한 알고리즘 분류"}}`
+  // 텍스트(키워드) 객관식 문제 출제
+
+  // 프롬프트 정의
+
+  const URL = session.images;
+  const urls = [];
+  var imageParts = [];
+  if (session.version == 'image') {
+    for(let i = 0; i < URL.length; i++){
+      urls[i] = fileToGenerativePart(URL[i], "image/png");
+  }
+  imageParts = [
+    urls[0], urls[1], urls[2], urls[3], urls[4], urls[5], urls[6], urls[7], urls[8], urls[9]
   ];
+  }
+  // 이미지 받은거 data url로 변환해서 넘기는 거까지
   
-  // const uploadedFile = request.files && request.files[0];
-  //   if (!uploadedFile) {
-  //     throw new Error("No file uploaded");
-  //   }
+    if (session.version == 'image') result = await model.generateContent([prompt, ...imageParts]);
+    else result = await model.generateContent([prompt]);
 
-  //   const imageParts = [
-  //     fileToGenerativePart(uploadedFile.content, uploadedFile.mimeType),
-  //   ];
-
-    const result = await model.generateContent([prompt, ...imageParts]);
     const response = await result.response;
     const text = response.text();
     console.log(text);
+  // gemini가 생성한 문제 value들
 
-    // let extractedData = response.text();
-    // console.log(extractedData);
-    // extractedData = extractedData.slice(extractedData.indexOf('{'), extractedData.lastIndexOf('}') + 1);
+    const jsonString = text.split('json')[1];
 
-    // console.log(extractedData)
-    // extractedData = JSON.parse(extractedData);
-    // console.log(extractedData);
+    const data = JSON.parse(jsonString);
+    console.log(data);
 
-    if (extractedData?.answer) {
-      extractedData.selections = {...extractedData.selections, answer: extractedData.answer}
-    }
-
-    if (extractedData.tag[1].includes(',')) {
-      const arr = extractedData.tag[1].split(",");
-      extractedData.tag = {};
-      for (var i = 0; i < arr.length; i++) {
-          let key = i + 1;
-          extractedData.tag[key] = arr[i].trim();
-      }
-    }
+    // 필요한 정보 추출
+    const t_title = data.title;
+    const t_description = data.description;
+    const t_selections = data.selections;
+    //const t_answer = data.answer;
+    const t_tag = data.tag;
 
     // POST 함수 호출
-
-    const {user_id, username} = getVerified();
-
+    const {username} = getVerified();
     const newQuiz = await prisma.snack_quiz.create({
         data: {
             snack_id: session.snack_id,
-            title: extractedData.title,
-            description: extractedData.description,
-            selections: extractedData.selections,
+            title: t_title,
+            description: t_description,
+            selections: t_selections,
             created_by: username,
-            tag: extractedData.tag,
+            tag: t_tag,
         },
     });
 
-    return Response.json({newQuiz})}
+    return Response.json(newQuiz)}
+
     catch(error){
         console.error('Error:', error);
 
@@ -91,122 +100,3 @@ export async function POST(request) {
         });
     }
 } 
-
-// export async function POST(request) {
-//     try {
-//         // Request에서 JSON 데이터 추출
-//         //const requestData = await request.json();
-//         console.log(extractedData);
-//         // 추출된 데이터를 활용하여 새로운 퀴즈 생성
-//         const newQuiz = await prisma.snack_quiz.create({
-//             data: {
-//                 snack_id: 1,
-//                 title: 'test',
-//                 description: 'testtest',
-//                 selections: 'test0103',
-//                 created_by: 'test',
-//                 tag: 'extractedData.tag',
-//             },
-//         });
-
-//         return new Response(JSON.stringify({ newQuiz }), {
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-//     } catch (error) {
-//         console.error('Error:', error);
-
-//         return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-//             status: 500,
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-//     }
-// }
-
-
-
-function extractInfoFromGPTResponse(gptResponse) {
-  console.log(gptResponse);
-  const lines = gptResponse.split('\n');
-  let choice1 = '';
-  let choice2 = '';
-  let choice3 = '';
-  let choice4 = '';
-  let title = '';
-  let description = '';
-  let selections = '';
-  let tag = '';
-
-  for (const line of lines) {
-    if (line.startsWith('문제 제목:')) {
-      title = line.slice(7).trim();
-    } else if (line.startsWith('문제 설명:')) {
-      description = line.slice(7).trim();
-    } else if (line.startsWith('1.')) {
-      choice1 = line.slice(3).trim();
-    } else if (line.startsWith('2.')) {
-      choice2 = line.slice(3).trim();
-    } else if (line.startsWith('3.')) {
-      choice3 = line.slice(3).trim();
-    } else if (line.startsWith('4.')) {
-      choice4 = line.slice(3).trim();
-    } else if (line.startsWith('정답:')) {
-      selections = line.slice(4).trim();
-    } else if (line.startsWith('관련 태그:')) {
-      tag = line.slice(7).trim();
-    }
-  }
-
-  return { title, description, choice1, choice2, choice3, choice4 , selections, tag };
-}
-
-/**
- * @swagger
- * /gpt:
- *   post:
- *     summary: OpenAI API를 이용한 스낵에 대한 새 스낵 퀴즈 생성
- *     description: OpenAI의 GPT-3.5-turbo 모델을 사용하여 주어진 세션 제목을 기반으로 새 퀴즈를 생성합니다.
- *     tags: [GPT]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               user_id:
- *                 type: int
- *                 example: 1
- *               snack_id:
- *                 type: int
- *                 example: 3
- *               title:
- *                 type: string
- *                 example: "트리의 특징"
- *     responses:
- *       200:
- *         description: 퀴즈가 성공적으로 생성
- *         content:
- *           application/json:
- *             example:
- *               newQuiz:
- *                 snack_quiz_id: 6
- *                 snack_id: 3
- *                 title: "퀴즈의 제목"
- *                 description: "생성된 퀴즈 설명"
- *                 selections:
- *                   1: "옵션 1"
- *                   2: "옵션 2"
- *                   3: "옵션 3"
- *                   4: "옵션 4"
- *                   answer: 2
- *                 created_by: 1
- *                 tag:
- *                   1: "알고리즘 카테고리 1"
- *                   2: "알고리즘 카테고리 2"
- *       500:
- *         description: Internal Server Error
- *         content:
- *           application/json:
- *             example:
- *               error: "Internal Server Error"
- */
