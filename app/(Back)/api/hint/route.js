9f985aa83977ce28946cc7a9c68784eb994a676d
haesoooo <98267078+haesoooo@users.noreply.github.com>
2024-02-25 20:37:22 +0900

    Update 힌트 프롬프트

--- app/(Back)/api/hint/route.js
+++ app/(Back)/api/hint/route.js
@@ -7,7 +7,7 @@ export async function POST(request) {
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     // 모델 정의
     var prompt = '';
-    prompt = `${session.code} \n 다음 코드에 대해 hint를 제공해줘. hint 내용에는 hint가 될 수 있는 함수나 code의 일부, 현재 내가 작성한 코드의 부족한 부분, 잘못된 부분, 진행 상황 등에 대해 전반적으로 feedback 해줘. 그러나 완전한 정답 코드를 제공해서는 안 돼. 코드에 대한 문제는 ${session.algorithm}.`
+    prompt = `${session.code} \n 다음 코드에 대해 hint를 제공해줘. hint 내용에는 hint가 될 수 있는 함수나 code의 일부, 현재 내가 작성한 코드의 부족한 부분, 잘못된 부분, 진행 상황에 대해 전반적으로 제공해줘. 그러나 완전한 정답 코드를 제공해서는 안 돼. 반드시 접근 방식이나 일부 코드에 대한 힌트만 제공해야 해. 코드에 대한 문제는 ${session.algorithm}.`
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
